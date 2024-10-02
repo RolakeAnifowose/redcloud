4b5ec2855cdda5608c4c932fe5acb49b06de602b
Rolake <morolaanney@gmail.com>
2024-10-02 01:28:58 +0100

    All files

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/v1/', methods=['GET'])
+def hello():
+    return 'Hello World'
+
+@app.route('/healthcheck/', methods=['GET'])
+def healthcheck():
+    return 'Healthy'
+
+# @app.route('/')
+# def no_page():
+#     return 'Invalid URL'
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return jsonify(error="Not found"), 404
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
